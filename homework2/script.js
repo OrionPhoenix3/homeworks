51335c2d282865f535ad096873d668446c73cfe8
OrionZVO <riddick.force@gmail.com>
2021-10-28 18:48:50 +0300

    homework2

---
+++ homework2/script.js
@@ -0,0 +1,23 @@
+//Zyabrev Vladislav
+
+//1
+
+let userAge =25;
+let userName ='Alice';
+let userSurname ='Stone';
+
+//2
+
+//newUser
+//shoppingList
+//statusСheck
+//employeeSalary
+//positionPointer
+
+//3
+
+let greetingsUser ='Hello World!'
+
+console.log(greetingsUser);
+
+console.log('userName',userName);
