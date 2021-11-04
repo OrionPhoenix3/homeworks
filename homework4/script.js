12bbd4dea96091e5ebe812101ab9b38abd8d93a9
OrionZVO <riddick.force@gmail.com>
2021-11-04 16:13:33 +0300

    homework4.1

---
+++ homework4/script.js
@@ -0,0 +1,51 @@
+// Zyabrev Vladislav JS3
+
+//Задание 1
+
+// let numberFirst = Number(prompt('Введите первое число:'));
+// let numberSecond = Number(prompt('Введите второе число:'));
+
+// function sum(a, b) {
+//     return a + b;
+//   }
+  
+//   let result = sum(numberFirst, numberSecond);
+//   alert( 'Сумма чисел: ' + result );
+//   let res0 = sum(9, 5);
+//   alert( 'Сумма чисел 9 и 5: ' + res0 );
+
+//   const sum = (numberFirst,numberSecond) => numberFirst + numberSecond;
+//   const res1 = sum(numberFirst,numberSecond);
+//   alert( 'Сумма чисел: ' + res1 );
+
+//Задание 2
+
+// function userGreetings(){
+//     let userName =prompt('Здравствуйте, напишите пожалуйста ваше имя пользователя:');
+//     let userAge = Number(prompt('Также укажите ваш возраст, пожалуйста:'));
+
+//     let message = (userAge > 30) ? 'Здравствуйте, ' + userName:
+//     (userAge <= 30) ? 'Привет, ' + userName :
+//     'Ввели неверные данные!';
+
+//     alert( message );
+//   }
+
+//   userGreetings();
+
+//Задание 3
+
+    // let firstNumber =Number(prompt('Введите число, которое хотите возвести в степень:'));
+    // let numberPower = Number(prompt('Введите степень:'));
+
+    // function exponentiation(a, b = 2) {
+    // return a**b;
+    
+    // }
+
+    // let result = exponentiation(firstNumber,numberPower);
+    // alert( result );
+    // let result1 = exponentiation(firstNumber);
+    // alert( result1 );
+
+
